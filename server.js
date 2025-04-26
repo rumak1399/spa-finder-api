bc22a54cd430acc8cafd9121399b7fb4625e391b
shovonroy173 <shovonroy2003@gmail.com>
2025-04-26 19:44:37 +0600

    mod: new test

--- server.js
+++ server.js
@@ -1,37 +1,53 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const connectDB = require('./config/db');
-const path = require('path');
-require('dotenv').config();
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+dotenv.config();
 
-// Initialize express app
 const app = express();
 
-// Connect to database
-connectDB();
+// import authRouter from "./routes/authRouter.js"
+// import userRouter from "./routes/userRouter.js"
+// import movieRouter from "./routes/movieRouter.js"
+// import paymentRouter from "./routes/paymentRouter.js"
+// import { verifyToken } from "./utils/verifyToken.js";
 
-// Middleware
+
+
 app.use(cors());
-app.use(express.json({ extended: false }));
-app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
 
-// Define routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/users', require('./routes/users'));
-app.use('/api/services', require('./routes/services'));
-app.use('/api/bookings', require('./routes/bookings'));
+mongoose.connect(process.env.MONGO_URI).then(()=>(console.log("Connected to Database!"))).catch(()=>("Not Connected to Database!"));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+app.get("/" , (req , res)=>{
+    res.status(200).send("Hello from server!");
+})
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// app.use("/api/auth" , authRouter);
+// app.use("/api/user" , userRouter);
+// app.use("/api/movie" , movieRouter);
+// app.use("/api/payment" , paymentRouter);
+// app.get("/token" , verifyToken)
 
-const PORT = process.env.PORT || 5000;
+app.use((err , req, res , next)=>{
+    console.log("LINE AT  21 INDEX" , err);
+    const errorStatus = err.status || 500;
+    const errorStack = err.stack;
+    const errorMessage = err.message || "Something broke!!";
+    res.status(errorStatus).json({
+        success:false , 
+        status : errorStatus , 
+        message : errorMessage , 
+        stack : errorStack
+    });
+});
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(process.env.PORT , ()=>{
+    try {
+        console.log("Connected to Server!");
+    } catch (error) {
+        console.log(error);
+    }
+})
