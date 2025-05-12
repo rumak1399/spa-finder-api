23cdafe326edc71f087d4659494c3b5b0fbc2bea
shovonroy173 <shovonroy2003@gmail.com>
2025-05-12 13:25:27 +0600

    mod: tag controller update

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -4,7 +4,22 @@ import Tag from "../models/Tag.js";
 export const addTag = async (req, res) => {
   try {
     const newTag = new Tag({ ...req.body });
-    await newTag.save();
+    const isPostId = await Tag.find({ postId: req.body.postId });
+    console.log(newTag, isPostId);
+    if (isPostId.length === 0) {
+      await newTag.save();
+    }
+    else{
+    await Tag.findByIdAndUpdate(
+      req.body.postId,
+      {
+        $addToSet: { tags: newTag?._id },
+      },
+      {
+        new: true,
+      }
+    );    
+    }
     // here add the logic to add this review in the post model
     await Post.findByIdAndUpdate(
       req.body.postId,
